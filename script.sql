d5f65b5ebc58c07d141f63211d43a77ce7675b4d
Fiderana-17 <hei.fiderana@gmail.com>
2025-11-13 09:50:26 +0300

    feat: Display the ID, first_name, and last_name of employees who have never taken leave in their lives.

--- script.sql
+++ script.sql
@@ -2,3 +2,10 @@ SELECT e.id, e.first_name, e.last_name
 FROM Employee e
 LEFT JOIN belong b ON e.id = b.employee_id
 WHERE b.team_id IS NULL;
+
+
+SELECT e.id, e.first_name, e.last_name
+FROM Employee e
+LEFT JOIN take t ON e.id = t.employee_id
+WHERE t.leave_id IS NULL;
+
