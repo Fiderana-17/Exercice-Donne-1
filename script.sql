3c98cf29e788536b48ea4f81f8e25a41a5a9198a
Fiderana-17 <hei.fiderana@gmail.com>
2025-11-14 11:26:10 +0300

    feat: Display vacations so that you can see the vacation ID, the start date of the vacation, the end date of the vacation, the first and last name of the employee taking the vacation, and the name of their team.

--- script.sql
+++ script.sql
@@ -9,3 +9,9 @@ FROM Employee e
 LEFT JOIN take t ON e.id = t.employee_id
 WHERE t.leave_id IS NULL;
 
+SELECT l.id AS leave_id, l.start_date, l.end_date, e.first_name, e.last_name, t.name AS team_name
+FROM Leave l
+JOIN take tk ON l.id = tk.leave_id
+JOIN Employee e ON tk.employee_id = e.id
+LEFT JOIN belong b ON e.id = b.employee_id
+LEFT JOIN Team t ON b.team_id = t.id;
